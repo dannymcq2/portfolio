eaccf71717fae98b365c16dc03bb679a4e05c06e
dannymcq2 <mcq.danny@icloud.com>
2024-11-11 17:57:04 -0500

    fix

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
+import nodemailer from 'nodemailer';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
+
+dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
-
 app.use(cors());
 app.use(bodyParser.json());
 
-// Serve static files from the `dist` directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Handle all routes and serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-// Example endpoint for your email functionality
 app.post('/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
@@ -31,7 +22,8 @@ app.post('/send-email', async (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
+    from: process.env.EMAIL_USER,
+    replyTo: email,
     to: process.env.EMAIL_USER,
     subject: `New message from ${name}`,
     text: message,
@@ -46,7 +38,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5006;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
